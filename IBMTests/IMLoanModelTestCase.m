0afade99828950947173dbd08f1b128b3e113dc7
Igor Ferreira <igorcferreira@hotmail.com>
2015-05-13 15:35:38 -0300

    Add other models test

--- IBMTests/IMLoanModelTestCase.m
+++ IBMTests/IMLoanModelTestCase.m
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import "IMLoan.h"
+#import "IMPaging.h"
+#import "IMLoanResponse.h"
 
 @interface IMLoanModelTestCase : XCTestCase
 
@@ -42,7 +44,7 @@
     
     XCTAssertNotNil(loanDictionary, @"Error on parse json");
     
-    IMLoan* loan = [[IMLoan alloc] initWithDictionary:loanDictionary];
+    IMLoan *loan = [[IMLoan alloc] initWithDictionary:loanDictionary];
     
     XCTAssertNotNil(loan, @"Error on parse");
     XCTAssertEqualObjects(loan.name, @"Flocerfida", @"Invalid Name");
@@ -51,6 +53,38 @@
     XCTAssertEqualObjects(loan.activity, @"General Store", @"Invalid Activity");
     XCTAssertEqualObjects(loan.sector, @"Retail", @"Invalid Sector");
     XCTAssertEqualObjects(loan.use, @"to buy items to sell like canned goods, personal care products, noodles, spices, coffee, milk, and other products.", @"Invalid Use");
+}
+
+- (void)testParsePagging {
+    NSDictionary *paggingDictionary = [self retrieveJsonInformationForFile:@"paging"];
+    XCTAssertNotNil(paggingDictionary, @"Error on parse json");
+    
+    IMPaging *paging = [[IMPaging alloc] initWithDictionary:paggingDictionary];
+    
+    XCTAssertNotNil(paging, @"Error on parse");
+    XCTAssertEqualObjects(paging.page, @1, @"Invalid page");
+    XCTAssertEqualObjects(paging.total, @5441, @"Invalid total");
+    XCTAssertEqualObjects(paging.pageSize, @20, @"Invalid page_size");
+    XCTAssertEqualObjects(paging.pages, @273, @"Invalid pages");
+}
+
+- (void)testParseLoanResponse {
+    NSDictionary *loanDictionary = [self retrieveJsonInformationForFile:@"valid"];
+    XCTAssertNotNil(loanDictionary, @"Error on parse json");
+    
+    IMLoanResponse *response = [[IMLoanResponse alloc] initWithDictionary:loanDictionary];
+    
+    XCTAssertNotNil(response, @"Error on parse");
+    
+    XCTAssertNotNil(response.paging, @"Invalid paging");
+    XCTAssertTrue([response.paging isKindOfClass:[IMPaging class]], @"Inavlid paging");
+    
+    XCTAssertNotNil(response.loans, @"Invalid loans");
+    XCTAssertTrue([response.loans count] == 20, @"Invalid loans");
+    
+    for(id object in response.loans) {
+        XCTAssertTrue([object isKindOfClass:[IMLoan class]]);
+    }
 }
 
 @end
