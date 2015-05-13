5d34a55b61dcfa6e25b88ee9e0e915f03f4f7314
Igor Ferreira <igorcferreira@hotmail.com>
2015-05-13 15:28:23 -0300

    Test loan parse

--- IBMTests/IMLoanModelTestCase.m
+++ IBMTests/IMLoanModelTestCase.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "IMLoan.h"
 
 @interface IMLoanModelTestCase : XCTestCase
 
@@ -21,6 +22,35 @@
 
 - (void)tearDown {
     [super tearDown];
+}
+
+-(NSDictionary*)retrieveJsonInformationForFile:(NSString*)file
+{
+    NSString *loanPath = [[NSBundle bundleForClass:[self class]] pathForResource:file ofType:@"json"];
+    NSData *loanData = [NSData dataWithContentsOfFile:loanPath];
+    
+    NSError *error;
+    NSDictionary *loanDictionary = [NSJSONSerialization JSONObjectWithData:loanData options:NSJSONReadingAllowFragments error:&error];
+    
+    XCTAssertNil(error,@"Error on parse json");
+    
+    return loanDictionary;
+}
+
+- (void)testParseLoan {
+    NSDictionary *loanDictionary = [self retrieveJsonInformationForFile:@"loan"];
+    
+    XCTAssertNotNil(loanDictionary, @"Error on parse json");
+    
+    IMLoan* loan = [[IMLoan alloc] initWithDictionary:loanDictionary];
+    
+    XCTAssertNotNil(loan, @"Error on parse");
+    XCTAssertEqualObjects(loan.name, @"Flocerfida", @"Invalid Name");
+    XCTAssertEqualObjects(loan.key, @883595, @"Invalid Id");
+    XCTAssertEqualObjects(loan.status, @"fundraising", @"Invalid Status");
+    XCTAssertEqualObjects(loan.activity, @"General Store", @"Invalid Activity");
+    XCTAssertEqualObjects(loan.sector, @"Retail", @"Invalid Sector");
+    XCTAssertEqualObjects(loan.use, @"to buy items to sell like canned goods, personal care products, noodles, spices, coffee, milk, and other products.", @"Invalid Use");
 }
 
 @end
